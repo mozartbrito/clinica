bd5dc630fe63fbf94dfc46ec43b8b1f279baabc8
taciobrito <tacio.7brito@gmail.com>
2017-06-08 09:44:13 -0300

    Ajustes

--- app/view/layout/footer.php
+++ app/view/layout/footer.php
@@ -2,8 +2,8 @@
 
 </div>
     <!-- jQuery (obrigatório para plugins JavaScript do Bootstrap) -->
-    <script src="js/jquery.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>js/jquery.min.js"></script>
     <!-- Inclui todos os plugins compilados (abaixo), ou inclua arquivos separadados se necessário -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>js/bootstrap.min.js"></script>
   </body>
 </html>
--- app/view/layout/header.php
+++ app/view/layout/header.php
@@ -8,7 +8,7 @@
     <title>Clínica CEO</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo BASE_URL; ?>css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim e Respond.js para suporte no IE8 de elementos HTML5 e media queries -->
     <!-- ALERTA: Respond.js não funciona se você visualizar uma página file:// -->
